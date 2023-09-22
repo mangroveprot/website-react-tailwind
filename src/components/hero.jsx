e1bb5ead01c6b3e6de3cf5551cc2783cb8515ad7
mangroveprot <mangroveprot@gmail.com>
2023-09-22 12:13:29 -0700

    Test commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ const hero = () => {
   return (
     <div className="text-white">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col">
-        <p>The only way to do great work is to love what you do.</p>
+        <p>The only way to do great work is to love what you do mean.</p>
       </div>
     </div>
   );
