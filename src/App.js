2a631c696eb64b132994b97a33a1a6da6184955e
mangroveprot <mangroveprot@gmail.com>
2023-09-22 11:42:11 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col">
+        <p>The only way to do great work is to love what you do.</p>
+      </div>
+    </div>
+  );
+};
+
+export default hero;
